8068236f14ee1cc2b50a71eec12eef0db990d92f
utkarshsharma1102 <104686350+utkarshsharma1102@users.noreply.github.com>
2022-04-30 21:49:52 +0530

    challenge looping

---
+++ challengeloops.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+int main() {
+    int i, j;
+    int count = 1;
+    for (i = 0; i < 6; i++) 
+    {
+        for (j = 0; j <= i; j++) 
+        {
+            printf("%d ", count);
+            count = !count;
+        }
+        count = i % 2;
+        printf("\n");
+    }
+    return(0);
+}
